d4ac8660bf3058adb27c113c980b3f3a2d1e143c
kylewuzhere <kyvasquez1026@gmail.com>
2024-01-23 14:43:26 +1300

    initialize express app; create routes; configure session

---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
+require('./auth.js');
+
+const indexRouter = require('./routes/indexRouter');
+const authRouter = require('./routes/authRouter');
+const companiesRouter = require('./routes/companyRouter');
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production' ? 'true' : 'auto',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', indexRouter);
+app.use('/auth', authRouter);
+app.use('/api/companies', companiesRouter);
+module.exports = app;
